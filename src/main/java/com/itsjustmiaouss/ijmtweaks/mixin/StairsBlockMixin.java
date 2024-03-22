0d578ede12fc20b4bc4c83bce5d39832f7b20c13
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2024-03-22 01:57:58 +0100

    feat: check if the next block is a valid stair
    - Shouldn't be waterlogged
    - Shouldn't be upside down
    - There should be a stair after the current one

--- src/main/java/com/itsjustmiaouss/ijmtweaks/mixin/StairsBlockMixin.java
+++ src/main/java/com/itsjustmiaouss/ijmtweaks/mixin/StairsBlockMixin.java
@@ -1,15 +1,20 @@
 package com.itsjustmiaouss.ijmtweaks.mixin;
 
+import com.itsjustmiaouss.ijmtweaks.IJMTweaks;
 import com.itsjustmiaouss.ijmtweaks.config.IJMTweaksConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.entity.Entity;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,6 +34,19 @@ public abstract class StairsBlockMixin {
 
         Block block = world.getBlockState(pos).getBlock();
         SimpleOption<Boolean> autoJump = MinecraftClient.getInstance().options.getAutoJump();
+
+        if(block instanceof StairsBlock) {
+            if(state.get(Properties.WATERLOGGED)) return;
+            if(state.get(Properties.BLOCK_HALF).equals(BlockHalf.TOP)) return; // Is the block upside down
+
+            Direction facing = state.get(HorizontalFacingBlock.FACING);
+            BlockPos offsetPos = pos.offset(facing).up();
+            Block upperBlock = world.getBlockState(offsetPos).getBlock();
+
+            // If the next upper block isn't a stair
+            if(!(upperBlock instanceof StairsBlock)) return;
+            IJMTweaks.LOGGER.info("enabled");
+        }
 
         autoJump.setValue(block instanceof StairsBlock);
     }
