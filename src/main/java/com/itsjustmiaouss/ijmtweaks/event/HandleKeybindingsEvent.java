d0313a0d96786777da54fb4d30e2d5bd597efc5d
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2024-08-24 22:06:51 +0200

    feat: fullbright send overlay message

--- src/main/java/com/itsjustmiaouss/ijmtweaks/event/HandleKeybindingsEvent.java
+++ src/main/java/com/itsjustmiaouss/ijmtweaks/event/HandleKeybindingsEvent.java
@@ -4,19 +4,31 @@ import com.itsjustmiaouss.ijmtweaks.config.IJMTweaksConfig;
 import com.itsjustmiaouss.ijmtweaks.keybind.IJMTweaksBindings;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 
 public class HandleKeybindingsEvent implements ClientTickEvents.EndTick {
+
+    private final MinecraftClient instance = MinecraftClient.getInstance();
 
     @Override
     public void onEndTick(MinecraftClient client) {
         if (IJMTweaksBindings.openConfigBinding.wasPressed()) {
-            MinecraftClient instance = MinecraftClient.getInstance();
             instance.setScreen(IJMTweaksConfig.getScreen().generateScreen(instance.currentScreen));
         }
 
         if (IJMTweaksBindings.fullbrightKeyBinding.wasPressed()) {
             IJMTweaksConfig config = IJMTweaksConfig.get();
             config.fullbright = !config.fullbright;
+
+            ClientPlayerEntity player = instance.player;
+            if (player != null) {
+                Text text = config.fullbright
+                        ? Text.translatable("overlay.ijmtweaks.fullbright.enabled")
+                        : Text.translatable("overlay.ijmtweaks.fullbright.disabled");
+                player.sendMessage(text, true);
+            }
         }
     }
 }
