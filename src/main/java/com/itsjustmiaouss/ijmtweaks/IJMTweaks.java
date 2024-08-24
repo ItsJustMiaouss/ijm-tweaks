fc0732906c5cbd41dd004ff4aa285b0eae117daa
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2024-08-24 19:12:19 +0200

    feat: add fullbright option with keybinding

--- src/main/java/com/itsjustmiaouss/ijmtweaks/IJMTweaks.java
+++ src/main/java/com/itsjustmiaouss/ijmtweaks/IJMTweaks.java
@@ -2,11 +2,12 @@ package com.itsjustmiaouss.ijmtweaks;
 
 import com.itsjustmiaouss.ijmtweaks.api.FabrishotIntegration;
 import com.itsjustmiaouss.ijmtweaks.config.IJMTweaksConfig;
+import com.itsjustmiaouss.ijmtweaks.event.HandleKeybindingsEvent;
 import com.itsjustmiaouss.ijmtweaks.keybind.IJMTweaksBindings;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,17 +19,17 @@ public class IJMTweaks implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        // Config
         IJMTweaksConfig.load();
+
+        // Keybindings
         IJMTweaksBindings.registerBindings();
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (IJMTweaksBindings.openConfigBinding.wasPressed()) {
-                MinecraftClient instance = MinecraftClient.getInstance();
+        // Events
+        ClientTickEvents.END_CLIENT_TICK.register(new HandleKeybindingsEvent());
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> IJMTweaksConfig.save());
 
-                instance.setScreen(IJMTweaksConfig.getScreen().generateScreen(instance.currentScreen));
-            }
-        });
-
+        // Fabrishot
         if (FabricLoader.getInstance().isModLoaded("fabrishot")) {
             FabrishotIntegration.load();
         }
