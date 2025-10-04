22322a640461509dcc2251a5c04d5cd184f9439f
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2025-10-04 11:52:12 +0200

    fix: bindings category name

--- src/main/java/com/itsjustmiaouss/ijmtweaks/keybind/IJMTweaksBindings.java
+++ src/main/java/com/itsjustmiaouss/ijmtweaks/keybind/IJMTweaksBindings.java
@@ -14,7 +14,7 @@ public class IJMTweaksBindings {
     public static KeyBinding openConfigBinding;
     public static KeyBinding fullbrightKeyBinding;
 
-    private static final KeyBinding.Category IJMTWEAKS_CATEGORY = KeyBinding.Category.create(Identifier.of(IJMTweaks.MOD_ID, "ijmtweaks"));
+    private static final KeyBinding.Category IJMTWEAKS_CATEGORY = KeyBinding.Category.create(Identifier.of(IJMTweaks.MOD_ID, "bindings"));
 
     public static void registerBindings() {
         zoomKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
